6c04ed743dd090018adc27ed2aa07d997a246197
charlyla <cnlaffitte@gmail.com>
2021-11-07 20:19:47 -0300

    add new jasypt fot application properties

--- src/main/java/com/transtour/backend/travel/BackendApplication.java
+++ src/main/java/com/transtour/backend/travel/BackendApplication.java
@@ -1,19 +1,17 @@
 package com.transtour.backend.travel;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-//@EnableAutoConfiguration
 @SpringBootApplication
-//@EnableSwagger2
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 @EnableFeignClients
 @EnableMongoRepositories(basePackages = "com.transtour.backend.travel.repository")
+@EnableEncryptableProperties
 public class BackendApplication {
 
 	public static void main(String[] args) {
