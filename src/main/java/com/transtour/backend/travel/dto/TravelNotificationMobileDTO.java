079bfd1b7294141ce98433d7d5e8e8342daea644
pabloOcanto <pomalianni@gmail.com>
2021-12-19 14:14:14 -0300

    cambio order number a long

--- src/main/java/com/transtour/backend/travel/dto/TravelNotificationMobileDTO.java
+++ src/main/java/com/transtour/backend/travel/dto/TravelNotificationMobileDTO.java
@@ -9,5 +9,5 @@ public class TravelNotificationMobileDTO {
 
     private String to;
     private Map<String, String> notification;
-    private Map<String, String> data;
+    private Map<String, Object> data;
 }
